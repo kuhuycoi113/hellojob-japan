b9b60a8d98e4b5c0001a32d5dd78c47913280690
ベトグェン <beto.hanoi@gmail.com>
2025-09-14 09:26:28 +0000

    Trong dữ liệu ứng viên Mức 1, trong dữ liệu “Địa điểm mong muốn” sẽ có c

---
+++ src/data/locations.ts
@@ -0,0 +1,69 @@
+// This is a new file.
+import type { Language } from "@/locales/translations";
+
+export type Prefecture = {
+  [key in Language]: string;
+};
+
+export type Region = {
+  [key in Language]: string;
+} & {
+  prefectures: Prefecture[];
+};
+
+export const japanRegions: Region[] = [
+  {
+    vi: "Hokkaido",
+    en: "Hokkaido",
+    ja: "北海道",
+    prefectures: []
+  },
+  {
+    vi: "Tohoku",
+    en: "Tohoku",
+    ja: "東北",
+    prefectures: []
+  },
+  {
+    vi: "Kanto",
+    en: "Kanto",
+    ja: "関東",
+    prefectures: []
+  },
+  {
+    vi: "Chubu",
+    en: "Chubu",
+    ja: "中部",
+    prefectures: []
+  },
+  {
+    vi: "Kansai",
+    en: "Kansai",
+    ja: "関西",
+    prefectures: []
+  },
+  {
+    vi: "Chugoku",
+    en: "Chugoku",
+    ja: "中国",
+    prefectures: []
+  },
+  {
+    vi: "Shikoku",
+    en: "Shikoku",
+    ja: "四国",
+    prefectures: []
+  },
+  {
+    vi: "Kyushu",
+    en: "Kyushu",
+    ja: "九州",
+    prefectures: []
+  },
+  {
+    vi: "Okinawa",
+    en: "Okinawa",
+    ja: "沖縄",
+    prefectures: []
+  }
+];
